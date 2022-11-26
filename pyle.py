93c1d5b200aeac06a5833a63c84f06d0f02dbd64
“viktorcvp” <dffggg2013@yandex.by>
2022-11-26 23:04:34 +0300

    pyles

---
+++ pyle.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая принимает на вход
+# вещественное число и показывает сумму его цифр.
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = float(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def sumNums(num):
+    sum = 0
+    for i in str(num):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+
+num = InputNumbers("Введите число: ")
+
+print(f"Сумма цифр = {sumNums(num)}")
