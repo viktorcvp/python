49838f716a30f4b47b06e0fb06c6c599def1a554
“viktorcvp” <dffggg2013@yandex.by>
2022-11-27 18:39:22 +0300

    n

--- pyle.py
+++ pyle.py
@@ -22,4 +22,32 @@ def sumNums(num):
 num = InputNumbers("Введите число: ")
 
 print(f"Сумма цифр = {sumNums(num)}")
-# new
+
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
